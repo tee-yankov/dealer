524d26e5c55ed3b4344c523a5e6cce614cb952c3
Teodor Yankov <tee.yankov@gmail.com>
2025-02-07 22:59:04 +0200

    playing field

--- src/components/playing-field.tsx
+++ src/components/playing-field.tsx
@@ -1,7 +1,26 @@
+import Card, { CardRank } from "./card";
+import "./playing-field.css";
+
 function PlayingField() {
+  const slots = 6;
+
   return (
-    <div />
-  )
+    <div className="playing-field-container">
+      {new Array(slots).fill(0).map((_, i) => (
+        <div key={i} className="nes-container is-rounded playing-field-slot">
+          <div className="playing-field-slot-content">
+            <Card
+              rank={
+                CardRank[
+                  Object.values(CardRank)[i] as CardRank
+                ] as unknown as CardRank
+              }
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default PlayingField
+export default PlayingField;
